40ca5a939cf27e3e82beb67e2038d0c86626ce7a
mariajalmeida <mari9d91@stud.kea.dk>
2021-02-09 14:45:41 +0100

    done

--- typewriter.js
+++ typewriter.js
@@ -12,15 +12,16 @@ function divideLetters() {
     const sentence = document.querySelector("h1").textContent;
 
     let i = 0;
-    let speed = 300;
+    let speed = 100;
 
     let timer = setInterval(function () {
         i++
         if (i <= sentence.length) {
             document.querySelector("h1").style.display = "block";
             document.querySelector("h1").textContent = sentence.substring(0, i);
-        } else if (i == sentence.length) {
+        } else {
             console.log("done");
+            clearInterval(timer);
         }
-    })
+    }, speed);
 }
